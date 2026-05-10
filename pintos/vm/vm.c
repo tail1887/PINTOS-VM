52d4267425f3b192625fb00dab1922ccaa69e704
이재혁 <cncnvmvm@naver.com>
2026-05-10 15:49:26 +0900

    vm_try_handle_fault 구현

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -173,14 +173,24 @@ vm_handle_wp (struct page *page UNUSED) {
 }
 
 /* Return true on success */
+//page fault 복구 가능성을 확인하고 실제 claim으로 넘기는 함수
 bool
-vm_try_handle_fault (struct intr_frame *f UNUSED, void *addr UNUSED,
-		bool user UNUSED, bool write UNUSED, bool not_present UNUSED) {
-	struct supplemental_page_table *spt UNUSED = &thread_current ()->spt;
-	struct page *page = NULL;
-	/* TODO: Validate the fault */
-	/* TODO: Your code goes here */
-
+vm_try_handle_fault (struct intr_frame *f UNUSED, void *addr ,
+		bool user UNUSED, bool write , bool not_present ) {
+	struct supplemental_page_table *spt  = &thread_current ()->spt;
+	if (addr == NULL || is_kernel_vaddr(addr)){
+		return false;
+	}
+	if (!not_present) {
+		return false;
+	}
+	struct page *page = spt_find_page(spt, addr);
+	if (page == NULL){
+		return false;
+	}
+	if (write && !page->writable){
+		return false;
+	}
 	return vm_do_claim_page (page);
 }
 
