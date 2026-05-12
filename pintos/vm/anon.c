62fc3a782ade6c96849d58be0d665854ff102105
Kyumin_Kim <kkm010412@gmail.com>
2026-05-12 17:27:43 +0900

    fix: add initializers return true

--- pintos/vm/anon.c
+++ pintos/vm/anon.c
@@ -29,8 +29,8 @@ bool
 anon_initializer (struct page *page, enum vm_type type, void *kva) {
 	/* Set up the handler */
 	page->operations = &anon_ops;
-
 	struct anon_page *anon_page = &page->anon;
+	return true;
 }
 
 /* Swap in the page by read contents from the swap disk. */
