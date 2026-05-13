8ea23f3001152954e13101ee6b5ae1c52de5d44c
이재혁 <cncnvmvm@naver.com>
2026-05-13 09:20:23 +0900

    중간저장

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -1241,6 +1241,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 }
 
 /* Create a PAGE of stack at the USER_STACK. Return true on success. */
+//초기 스택페이지를 만들고, frame과 매핑
 static bool
 setup_stack (struct intr_frame *if_) {
 	bool success = false;
